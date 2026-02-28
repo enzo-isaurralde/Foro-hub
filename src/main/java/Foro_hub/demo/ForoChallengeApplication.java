f30ede3b451ff247b445123f8cd7c5eae2f38d9b
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-02-28 20:26:08 -0300

    base con dependencias

---
+++ src/main/java/Foro_hub/demo/ForoChallengeApplication.java
@@ -0,0 +1,13 @@
+package Foro_hub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoChallengeApplication.class, args);
+	}
+
+}
